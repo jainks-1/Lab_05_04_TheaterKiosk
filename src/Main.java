9a3c1feb5b261157aebe0e242f32db4057a83a74
jainks-1 <jainks@mail.uc.edu>
2025-02-16 16:38:10 -0500

    fixed comment for determine age

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,47 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+// Kaden Jain
+// Computer Programming 1 - Spring 2025
+/* As people pass through an entry kiosk at the theater,
+they are prompted to enter their age. If they are 21 or
+older, they get a paper wristband. Code a logic program
+that asks the user to enter their age and then if they are
+21 or over displays a message that they get a wristband.
+Note that the program does nothing if they are not 21 or over…
+ */
+
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        // declare variables
+        Scanner in = new Scanner(System.in);
+        int userAge = 0;
+        String trash = "";
+
+        // input validation if/else
+        System.out.println("Welcome to the theater! Please enter your age in years: ");
+        if (in.hasNextInt()) {
+            userAge = in.nextInt();
+            in.nextLine();
+
+            // determine age if/else
+            if (userAge >= 21){
+
+                System.out.println("You are 21 or older. You get a wristband.");
+
+            }
+            else {
+
+                System.out.println("Thank you, please proceed.");
+
+            }
+
         }
+        else {
+            trash = in.nextLine();
+            System.out.println("You entered an invalid input: " + trash);
+            System.out.println("Please run the program again");
+            System.exit( 0);
+        }
+
     }
 }
